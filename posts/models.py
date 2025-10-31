b5c7dcfd2b6fd89f561e6dd235690f9331c2b5b5
Rohit Singh <rsingh.py@gmail.com>
2025-10-31 08:10:24 +0530

    models added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=60, blank=False)
+    post_content = models.TextField()
+    publish_date = models.DateField(auto_now=True)
